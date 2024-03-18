948849bb0f3c1680eeb088b4cff1a0b3fcc17087
Doug Kellermeyer <doug.kellermeyer@gmail.com>
2024-03-18 11:22:52 -0400

    cleaned up spacing

--- src/components/main.js
+++ src/components/main.js
@@ -121,7 +121,7 @@ const Main = () => {
             <section class="sectionWrapper">
                 <span className={classes.scrollToSectionOne} id="sectionOne"></span>
                 <h1 class="jobHeader">The traditional job search process is broken.</h1>
-                <h2 style={{marginBottom: "100px", textAlign: "center"}}><span style={{color: "#9700ff", fontWeight: "bold"}}>Connections land jobs</span>, not online applications.</h2>
+                <h2 class="connectionsText"><span style={{color: "#9700ff", fontWeight: "bold"}}>Connections land jobs</span>, not online applications.</h2>
             </section>
             <span className={classes.scrollToSectionOne} id="sectionTwo"></span>
 
@@ -172,7 +172,7 @@ const Main = () => {
                     </div>
                     <div class="pricingPlan">
                         <h2 class="pricingTitle">Plus</h2>
-                        <p class="pricingSubtitle">Get latest leads, updated monthly</p>
+                        <p class="pricingSubtitle">Get latest leads, updated weekly</p>
                         <p class="pricingNumber">$10<span style={{ fontSize: "1rem", color: "black" }}>/month</span></p>
                         <div>
                             <div class="pricingTextIcon">
